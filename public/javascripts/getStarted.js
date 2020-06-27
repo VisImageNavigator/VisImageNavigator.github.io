73da83afa04ba97d093a51cacc18010551ff231b
LiruiErnest <li.8950@osu.edu>
2020-06-27 13:20:43 -0400

    v2.0
    V2.0

---
+++ public/javascripts/getStarted.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Rui Li
+ * @Date: 2020-06-27 10:04:57
+ * @LastEditTime: 2020-06-27 10:12:05
+ * @Description: 
+ * @FilePath: /VisImageNavigator.github.io/public/javascripts/getStarted.js
+ */ 
+$(document).ready(function () {
+    // $("#total-img-div").load("public/images/total-img-by-year.html");
+});
