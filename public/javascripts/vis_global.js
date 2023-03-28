3700c583ce4bf24917406fef3618fddee9cb114f
VisImageNavigator <66805489+VisImageNavigator@users.noreply.github.com>
2023-03-28 09:30:37 -0400

    V3.0.25
    03/17 requested data
    using data from 03/25

--- public/javascripts/vis_global.js
+++ public/javascripts/vis_global.js
@@ -75,7 +75,7 @@ $(document).ready(function () {
  */
 async function dbStart() {
 
-    G_IMG_DATA = await d3.csv("public/dataset/vispubData30_updated_02212023.csv");
+    G_IMG_DATA = await d3.csv("public/dataset/vispubData30_updated_03172023.csv");
     G_PAPER = await d3.csv("public/dataset/paperData_3.0.3.csv");
     //G_PAPER = stratifyPaperData(G_PAPER);
     G_IMG_DATA = sortImageByYear(G_IMG_DATA); //sort images by year, then sort by conference, the sort by first page.
